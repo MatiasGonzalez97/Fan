2a243e8fe8062c59ce9e7469215fd6dbaf7ec7fa
MGCarry <matiaspch@gmail.com>
2017-12-12 14:18:42 -0300

    Add files via upload

---
+++ DC Legends Of Tomorrow/foro.php
@@ -0,0 +1,41 @@
+<?php
+	include("conexionBD.php");
+	if(isset($_GET["id"]))
+	$id = $_GET['id'];
+	$query = "SELECT * FROM  foro WHERE ID = '$id' ORDER BY fecha DESC";
+	$result = $mysqli->query($query);
+	while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		$id = $row['ID'];
+		$titulo = $row['titulo'];
+		$autor = $row['autor'];
+		$mensaje = $row['mensaje'];
+		$fecha = $row['fecha'];
+		$respuestas = $row['respuestas'];
+
+		echo "<tr><td>$titulo</tr></td>";
+		echo "<table>";
+		echo "<tr><td>autor: $autor</td></tr>";
+		echo "<tr><td>$mensaje</td></tr>";
+		echo "</table>";
+		echo "<br /><br /><a href='formulario.php?id&respuestas=$respuestas&identificador=$id'>Responder</a><br />";
+	}
+
+	$query2 = "SELECT * FROM  foro WHERE identificador = '$id' ORDER BY fecha DESC";
+	$result2 = $mysqli->query($query2);
+	echo "<br />respuestas:<br /><br />";
+	while($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
+		$id = $row['ID'];
+		$titulo = $row['titulo'];
+		$autor = $row['autor'];
+		$mensaje = $row['mensaje'];
+		$fecha = $row['fecha'];
+		$respuestas = $row['respuestas'];
+
+		echo "<tr><td>$titulo</tr></td>";
+		echo "<table>";
+		echo "<tr><td>autor: $autor</td></tr>";
+		echo "<tr><td>$mensaje</td></tr>";
+		echo "</table>";
+		echo "<br /><br /><a href='formulario.php?id&respuestas=$respuestas&identificador=$id'>Responder</a><br />";
+	}
+?>
---
+++ Flash/formulario.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+//si hay una sesión
+if (isset($_SESSION['name'])){
+    //se muestra el contenido de la página web
+	if(isset($_GET["respuestas"]))
+		$respuestas = $_GET['respuestas'];
+	else
+		$respuestas = 0;
+	if(isset($_GET["identificador"]))
+		$identificador = $_GET['identificador'];
+	else
+		$identificador = 0;
+?>
+<table>
+<form name="form" action="agregar.php" method="post">
+	<input type="hidden" name="identificador" value="<?php echo $identificador;?>">
+	<input type="hidden" name="respuestas" value="<?php echo $respuestas;?>">
+    <tr>
+	<a>Autor:  <?php print $_SESSION['name'];?></a>
+    </tr>
+    <tr>
+      <td>Titulo</td>
+      <td><input type="text" name="titulo"></td>
+    </tr>
+    <tr>
+      <td>Mensaje</td>
+      <td><textarea name="mensaje" cols="50" rows="5" required="required"></textarea></td>
+    </tr>
+    <tr>
+      <td><input type="submit" id="submit" name="submit" value="Enviar Mensaje"></td>
+    </tr>
+  </form>
+</table>
+<?php
+}//si no hay sesión
+else{
+//se redirecciona
+header ('location: ./');
+}
+?>
